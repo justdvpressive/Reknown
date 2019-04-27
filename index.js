b27192e9901b53ce18b4ea5e7e174a9f15372130
justdvpressive <46937250+justdvpressive@users.noreply.github.com>
2019-04-27 13:32:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const Client = require('./structures/client.js');
 
+const BOT_TOKEN = "NTcxNDY4OTI4MTkxMTY4NTEz.XMQ9ng.EBIiQmwx8Ye3ajLwLUvKY7ouDIM";
 process.on('unhandledRejection', error => {
   console.error(error);
   return Client.rollbar.error(error);
 });
 
-Client.bot.login(process.env.BOT_TOKEN);
+Client.bot.login(BOT_TOKEN);
